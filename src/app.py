675f68ffd80b409f5abddb2eb9d1163fa8599e58
LeLapinouAlpha <t-henon@hotmail.fr>
2024-05-05 21:34:40 +0200

    feat: implement an hello <user> route

--- src/app.py
+++ src/app.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello World!</p>"
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/<name>")
+def hello_user(name):
+    return f"<p>Hello, {escape(name)}!</p>"
