5f5ccfe2e92e86e959a9e3b6ac4b1f534848a4f6
LeLapinouAlpha <t-henon@hotmail.fr>
2024-05-06 17:56:53 +0200

    refactor(app): remove all endpoints

--- src/app.py
+++ src/app.py
@@ -9,13 +9,3 @@ db.create_tables([User])
 
 # Initialize flask application
 app = Flask(__name__)
-
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-
-@app.route("/<name>")
-def hello_user(name):
-    return render_template("hello.html", name=name)
