278e117a65820547914d5083c8f7704f30365f74
LeLapinouAlpha <t-henon@hotmail.fr>
2024-05-06 15:39:35 +0200

    feat: initialize database and create tables at start

--- src/app.py
+++ src/app.py
@@ -1,5 +1,13 @@
 from flask import Flask, render_template
 
+from database.user import User
+from database.config import db
+
+# Initialize database connection and tables
+db.connect()
+db.create_tables([User])
+
+# Initialize flask application
 app = Flask(__name__)
 
 
