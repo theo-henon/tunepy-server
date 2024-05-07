c08d40a992457dfc57383da735b77f4c8e46e36d
LeLapinouAlpha <t-henon@hotmail.fr>
2024-05-07 21:57:21 +0200

    refactor: unify JSON response's message field

--- src/app.py
+++ src/app.py
@@ -25,11 +25,11 @@ def user_register():
         username, password = request.json["username"], request.json["password"]
         User.create(username=username, password=bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()))
     except KeyError:
-        return {"error": "Missing credentials."}, 400
+        return {"msg": "Missing credentials."}, 400
     except DoesNotExist:
-        return {"message": f"The user '{username}' has been successfully registered."}, 200
+        return {"msg": f"The user '{username}' has been successfully registered."}, 200
     except IntegrityError:
-        return {"error": "This username is already taken."}, 409
+        return {"msg": "This username is already taken."}, 409
 
 
 @app.route("/users/login", methods=["POST"])
@@ -40,13 +40,13 @@ def user_login():
         user = User.get(User.username == username)
         if bcrypt.checkpw(password.encode("utf-8"), user.password.encode("utf-8")):
             access_token = create_access_token(identity=user.id)
-            return {"message": f"User '{username}' successfully logged in.", "access_token": access_token}, 200
+            return {"msg": f"User '{username}' successfully logged in.", "access_token": access_token}, 200
         else:
-            return {"error": "Invalid password."}, 400
+            return {"msg": "Invalid password."}, 400
     except KeyError:
-        return {"error": "The username or the password is missing"}, 400
+        return {"msg": "The username or the password is missing"}, 400
     except DoesNotExist:
-        return {"error": f"The user '{username}' does not exist."}, 404
+        return {"msg": f"The user '{username}' does not exist."}, 404
 
 
 @app.route("/users/profile/<username>", methods=["GET", "PUT"])
@@ -57,7 +57,7 @@ def user_profile(username):
             user = User.get(User.username == username)
             return {"id": user.id, "username": user.username}
         except DoesNotExist:
-            return {"error": f"User '{username}' not found."}, 404
+            return {"msg": f"User '{username}' not found."}, 404
     else:
         user_id = get_jwt_identity()
         user = None
@@ -68,11 +68,11 @@ def user_profile(username):
                 user.username = new_username
                 user.save()
             else:
-                return {"error": f"You're not authorized to update {username}'s profile."}, 401
+                return {"msg": f"You're not authorized to update {username}'s profile."}, 401
         except DoesNotExist:
-            return {"error": f"User '{username}' not found."}, 404
+            return {"msg": f"User '{username}' not found."}, 404
         except IntegrityError:
-            return {"error": "This username is already taken."}, 409
+            return {"msg": "This username is already taken."}, 409
         except KeyError as key_error:
             pass
         return {"id": user.id, "username": user.username}
