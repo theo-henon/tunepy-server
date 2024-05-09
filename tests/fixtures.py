4037c2b092e58ad56521ab15fdba877ef9be2eed
LeLapinouAlpha <t-henon@hotmail.fr>
2024-05-09 14:23:38 +0200

    tests: remove 'test_app' fixture

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -13,12 +13,6 @@ def test_reset_app():
 
 
 @pytest.fixture
-def test_app():
-    app.config.update({"TESTING": True})
-    yield app
-
-
-@pytest.fixture
 def client(test_reset_app):
     return app.test_client()
 
